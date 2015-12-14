b5855cf8f72b563c2d54231d2547d53ee300268a
Thomas Beirne <thomas.beirne@gmail.com>
2015-12-13 19:49:21 -0800

    Remove unused file

--- src/util/get-sorted-active-idxs.js
+++
@@ -1,6 +0,0 @@
-import _ from 'lodash'
-
-export default function getSortedActiveIdxs(activeIdxMap) {
-	const activeIdxs = Object.keys(activeIdxMap).map(x => parseInt(x, 10))
-	return _.sortBy(activeIdxs)
-}
